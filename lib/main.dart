15201fa7be9f9143df75088a40493ebe9345d9ed
jojoooook <89437396+jojoooook@users.noreply.github.com>
2025-03-07 11:05:57 +0700

    add screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/screens/home_screen.dart';
+import 'package:pilem/screens/search_screen.dart';
+import 'package:pilem/screens/favorite_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,10 +13,49 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pilem',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Pilem',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const MainScreen());
   }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const FavoriteScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,     
+        items: const 
+          <BottomNavigationBarItem> [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'), 
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'), 
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
+            ]),
+    )
+      )
+    }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/screens/detail_screen.dart';
 import 'package:pilem/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,13 +17,18 @@ class _HomeScreenState extends State<HomeScreen> {
   List<Movie> _trendingMovies = [];
   List<Movie> _popularMovies = [];
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadMovies();
+  }
+
   Future<void> _loadMovies() async {
     final List<Map<String, dynamic>> allMoviesData =
         await _apiService.getAllMovies();
-
     final List<Map<String, dynamic>> trendingMoviesData =
         await _apiService.getTrendingMovies();
-
     final List<Map<String, dynamic>> popularMoviesData =
         await _apiService.getPopularMovies();
 
@@ -35,46 +41,36 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void initState() {
-    // TODO: IMPLEMENT initState
-    super.initState();
-    _loadMovies();
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pilem'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildMovieList('All Movies', _allMovies),
-            _buildMovieList('Trending Movies', _trendingMovies),
-            _buildMovieList('Popular Movies', _popularMovies),
-          ],
+        appBar: AppBar(
+          title: const Text('Pilem'),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildMovieList('All Movies', _allMovies),
+              _buildMovieList('Trending Movies', _trendingMovies),
+              _buildMovieList('Popular Movies', _popularMovies),
+            ],
+          ),
+        ));
   }
 
   Widget _buildMovieList(String title, List<Movie> movies) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // Judul
+        //Judul
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
             title,
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
         ),
-        // Movie
+        //Movie
         SizedBox(
           height: 200,
           child: ListView.builder(
@@ -83,36 +79,33 @@ class _HomeScreenState extends State<HomeScreen> {
             itemBuilder: (context, index) {
               final Movie movie = movies[index];
               return GestureDetector(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Image.network(
-                        'https://image.tmdb.org/t/p/w500${movie.posterPath}',
-                        height: 150,
-                        width: 100,
-                        fit: BoxFit.cover,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        movie.title.length > 14
-                            ? '${movie.title.substring(0, 10)}...'
-                            : movie.title,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailScreen(movie: movie))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                          height: 150,
+                          width: 100,
+                          fit: BoxFit.cover,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
+                        const SizedBox(height: 5),
+                        Text(
+                          movie.title.length > 14
+                              ? '${movie.title.substring(0, 10)}...'
+                              : movie.title,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ));
             },
           ),
-        ),
+        )
       ],
     );
   }
